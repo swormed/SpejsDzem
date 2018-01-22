b968e4748afc9473c878838f16919f3c12688ba3
swormed <sworm@vp.pl>
2018-01-22 20:20:53 +0100

    dodanie menu kategorii

---
+++ Assets/Scripts/loadKategorie.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class loadKategorie : MonoBehaviour
+{
+
+    public Button butt;
+
+    void Start()
+    {
+        Button btn = butt.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.LoadLevel("kategorie");
+    }
+
+}
